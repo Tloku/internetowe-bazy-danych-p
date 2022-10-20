9a78632b523bc9d275c7312721900ff6f8d10333
Tloku <tlokinski.dominik@gmail.com>
2022-10-20 22:47:25 +0200

    Sql script not finished

--- Backend/gym-plan-server/src/main/resources/db/migration/V1__createTables.sql
+++ Backend/gym-plan-server/src/main/resources/db/migration/V1__createTables.sql
@@ -17,25 +17,23 @@ CREATE TYPE DIFFICULTY AS ENUM (
 )
 
 CREATE TABLE EXERCISE (
-    id int,
-    name varchar(255) NOT NULL ,
+    id serial primary key,
+    name varchar(255) unique NOT NULL ,
     muscle_group MUSCLE_GROUP NOT NULL ,
     description varchar(2000) NOT NULL ,
     difficulty DIFFICULTY NOT NULL ,
     url varchar(255) NOT NULL,
-    unique (name)
 )
 
 CREATE TABLE USER (
-    id int,
-    login varchar(255) NOT NULL,
+    id serial primary key ,
+    login varchar(255) unique NOT NULL,
     password varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
-    UNIQUE (login)
 )
 
 CREATE TABLE PLAN (
-    id int,
+    id serial primary key,
     date_from datetime,
     date_to datetime,
     name varchar(255),
@@ -43,9 +41,11 @@ CREATE TABLE PLAN (
 )
 
 CREATE TABLE EXERCISE_WITH_REPS (
-    id ,
+    id serial primary key ,
     reps int,
     series int,
+    FOREIGN KEY (exercise_id)
+        REFERENCES EXERCISE (id)
 )
 
 
